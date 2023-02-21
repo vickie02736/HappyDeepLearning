e1c347f18a616f7224659d990de2175f6d3a8d5e
vickie02736 <106783002+vickie02736@users.noreply.github.com>
2023-02-21 22:41:49 +0000

    Add files via upload

--- useTool.py
+++ useTool.py
@@ -126,3 +126,17 @@ def evaluate_loss(net, data_iter, loss):
         l = loss(out, y)
         metric.add(l.sum(), l.numel())
     return metric[0] / metric[1]
+
+
+
+def try_gpu(i=0): 
+    """如果存在，则返回gpu(i)，否则返回cpu()"""
+    if torch.cuda.device_count() >= i + 1:
+        return torch.device(f'cuda:{i}')
+    return torch.device('cpu')
+
+def try_all_gpus(): 
+    """返回所有可⽤的GPU，如果没有GPU，则返回[cpu(),]"""
+    devices = [torch.device(f'cuda:{i}')
+        for i in range(torch.cuda.device_count())]
+    return devices if devices else [torch.device('cpu')]
